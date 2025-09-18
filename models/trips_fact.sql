7d5a358833f6c48c5ea636bff913009aa351cd3c
Saswati Mishra <Saswati.Mishra@perficient.com>
2025-09-18 05:32:36 +0000

    trip fact model created

---
+++ models/trips_fact.sql
@@ -0,0 +1,16 @@
+WITH trips as (
+    select 
+    ride_id,
+    RIDEABLE_TYPE,
+    DATE(TRY_TO_TIMESTAMP(STARTED_AT)) AS TRIP_DATE,
+    start_statio_id,
+    end_station_id,
+    MEMBER_CSUAL AS MEMBER_CASUAL,
+    TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(ENDED_AT),TO_TIMESTAMP(STARTED_AT)) AS Trip_Duration_seconds,
+    from 
+    {{ source ('demo','bike')}}
+    where ride_id!='ride_id' 
+    --limit 10   
+
+)
+select * from trips 
