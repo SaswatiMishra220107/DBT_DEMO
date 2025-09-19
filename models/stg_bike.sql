8b68f3d79c5e4015a08f698847e22dee73a7df10
Saswati Mishra <Saswati.Mishra@perficient.com>
2025-09-19 05:26:51 +0000

    bike stg file updated

--- models/stg_bike.sql
+++ models/stg_bike.sql
@@ -1,19 +1,25 @@
-WITH Bike AS(
-    SELECT 
-            RIDE_ID,
-			REPLACE(STARTED_AT,'"','') AS STARTED_AT,
-			REPLACE(ENDED_AT,'"','') AS ENDED_AT,
-			START_STATION_NAME,
-			START_STATIO_ID,
-			END_STATION_NAME,
-			END_STATION_ID,
-			START_LAT ,
-			START_LNG ,
-			END_LAT ,
-			END_LNG ,
-			MEMBER_CSUAL
-    FROM {{ source("demo", "bike") }}      
-    WHERE RIDE_ID!='bikeid'
+WITH BIKE AS (
+
+select
+RIDE_ID,
+REPLACE(STARTED_AT,'"','') AS STARTED_AT,
+REPLACE(ENDED_AT,'"','') AS ENDED_AT,
+START_STATION_NAME,
+START_STATIO_ID,
+END_STATION_NAME,
+END_STATION_ID,
+START_LAT,
+START_LNG,
+END_LAT,
+END_LNG,
+MEMBER_CSUAL
+
+from {{ source('demo', 'bike') }}
+
+where RIDE_ID not in ('"bikeid"', '""bikeid""') 
+  and STARTED_AT not in ('"starttime"', 'starttime', '""starttime""')
 )
 
-SELECT * FROM bike
+select
+*
+from BIKE
